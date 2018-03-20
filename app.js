7b6dac287e6f6462adb2edd123e87e5150739538
JSmith <jaysmit158@gmail.com>
2018-03-19 20:42:22 -0400

    Created node server

---
+++ app.js
@@ -0,0 +1,31 @@
+//Problem: We need a simple way to look at a user's badge count and Javascript point from a web browser
+
+//Solution: Use nodejs to perform the profile look ups and serve our template via HTTP.
+
+
+//1. Create a web server
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('Hello World!\n');
+    response.end();
+}).listen(8080);
+
+//2. Handle HTTP route GET / AND POST / i.e. Home
+    //if url == "/" && GET
+      //show search
+    // if url == "/" && POST
+      //redirect to /:username
+
+//3. Handle HTTP route GET /:usernmae ie. /chalkers
+  //if url == "/...."
+    //get json from Treehouse
+      //on "end"
+        //show profile
+      //on "error"
+        //show error
+
+//4. Function that handles the reading of files and merge in value
+    //read from file and get string
+      //merge values in to string
