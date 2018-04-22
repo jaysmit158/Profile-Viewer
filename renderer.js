a5e4b893362aa7119b8a3f3aff471d63b70cac7c
JSmith <jaysmit158@gmail.com>
2018-04-22 01:03:49 -0400

    added filesystem

--- renderer.js
+++ renderer.js
@@ -1,3 +1,15 @@
- //4. Function that handles the reading of files and merge in value
-    //read from file and get string
-      //merge values in to string
+var fs = require("fs");
+
+function view(templateName, values, response) {
+	//Read from the template file
+	fs.readFile('/views/', function(err,data){
+		if (err) throw err;
+
+	//Insert values in to the content
+	//Write out to the response
+	console.log(data);
+	});
+
+
+	
+}
